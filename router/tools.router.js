3be351ba9961ba12646355ebad11f78226866f3d
Abinithishkumarprabakaran <abinithishkumarp.a.k@gmail.com>
2023-04-02 12:49:18 +0530

    Project-Creation

--- router/tools.router.js
+++ router/tools.router.js
@@ -1,9 +1,14 @@
 import express from "express";
+import { createProject } from "../service/tools.service.js";
 
 const router = express.Router();
 
-router.post("/", async function (request, response) {
-    const { projectTitle, projectDescription } = request.body
+router.post("/projectCreation", async function (request, response) {
+    const data = request.body
+    console.log(data)
+    const res = await createProject(data)
+
+    response.send(res)
 })
 
 export default router
--- service/tools.service.js
+++ service/tools.service.js
@@ -1,2 +1,9 @@
 import { ObjectId } from "mongodb";
 import { client } from "../index.js";
+
+export async function createProject(data) {
+    return await client
+        .db(`${data.projectTitle}`)
+        .collection("Description")
+        .insertOne(data);
+}
