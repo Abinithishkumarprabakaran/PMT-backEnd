5fccf1a2fea204ebf29fe9de11e6df514685a9b9
Abinithishkumarprabakaran <abinithishkumarp.a.k@gmail.com>
2023-04-02 23:15:24 +0530

    backend

--- router/tools.router.js
+++ router/tools.router.js
@@ -1,12 +1,26 @@
 import express from "express";
-import { createProject } from "../service/tools.service.js";
+import { createProject, projectTitles, getProjectTitles, getAllProjectTitles } from "../service/tools.service.js";
 
 const router = express.Router();
 
 router.post("/projectCreation", async function (request, response) {
-    const data = request.body
-    console.log(data)
-    const res = await createProject(data)
+    const {projectTitle, projectDescription} = request.body
+
+    const ProjectFromDB = await getProjectTitles(projectTitle);
+    console.log(ProjectFromDB)
+
+    if(ProjectFromDB) {
+        response.status(404).send({message: "Project Already Exists"})
+    }
+    else{
+        const res = await projectTitles(request.body)
+        response.send(res)
+    }
+})
+
+router.get("/dashboard", async function (request, response) {
+
+    const res = await getAllProjectTitles()
 
     response.send(res)
 })
--- service/tools.service.js
+++ service/tools.service.js
@@ -7,3 +7,25 @@ export async function createProject(data) {
         .collection("Description")
         .insertOne(data);
 }
+
+export async function projectTitles(data) {
+    return await client
+        .db("ProjectTitles")
+        .collection("titles")
+        .insertOne(data)
+}
+
+export async function getProjectTitles(projectTitle) {
+    return await client
+        .db("ProjectTitles")
+        .collection("titles")            
+        .findOne({projectTitle: projectTitle})
+}
+
+export async function getAllProjectTitles() {
+    return await client
+        .db("ProjectTitles")
+        .collection("titles")            
+        .find({})
+        .toArray();
+}
